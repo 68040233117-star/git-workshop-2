c4df4506176807ee7e9ea353df5553fad0a902a7
Michiel <68040233117@udru.ac.th>
2026-01-17 14:50:44 +0700

    add script roblox

---
+++ ScriptRoblox.lua
@@ -0,0 +1,15 @@
+local prompt = script.Parent:WaitForChild("ProximityPrompt")
+local tool_meat = game.ServerStorage:WaitForChild("meet1")
+
+prompt.Triggered:Connect(function(player)
+	
+	if player.Character and not player.Backpack:FindFirstChild(tool_meat.Name) 
+		and not player.Character:FindFirstChild(tool_meat.Name) then
+		
+		local tool1 = tool_meat:Clone()
+		
+		player.Character.Humanoid:EquipTool(tool1)
+		
+
+	end
+end)
